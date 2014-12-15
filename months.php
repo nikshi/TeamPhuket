f1b2a880eaa12c8222ebb66767c8556354f814a8
Filip Kolev <fil.kolev@gmail.com>
2014-12-15 16:49:29 +0200

    Minor fixes in CSS files - removed duplicate properties, removed "px" after 0; code formatting Fixed name in header and footer - Team Phuket; Removed redundant character escapes from regex in js file;

--- months.php
+++ months.php
@@ -1,8 +1,8 @@
 <div class="post-lists">
     <h2>Months</h2>
     <ul class="nav nav-tabs">
-        <li><a href="#">January(2)</a></li>
-        <li><a href="#">March(7)</a></li>
+        <li><a href="months.php">January(2)</a></li>
+        <li><a href="months.php">March(7)</a></li>
         <li><a href="#">September(1)</a></li>
         <li><a href="#">Octomber(4)</a></li>
         <li><a href="#">November(5)</a></li>
@@ -14,3 +14,13 @@
         <li><a href="#">November(5)</a></li>
     </ul>
 </div>
+
+<?php
+
+require 'config.php';
+$monthQuery = "SELECT title FROM posts WHERE MONTH(date) = 12 AND YEAR(date) = 2014";
+$arr = $con->query($monthQuery);
+var_dump($arr->fetch_all());
+
+
+?>
