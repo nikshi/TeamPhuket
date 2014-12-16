eec2f133770d6f406f7bf49a1f69def1d53ce5a7
nikshi <n.shishmanov@abv.bg>
2014-12-16 11:57:35 +0200

    edited tags module

--- tags.php
+++ tags.php
@@ -1,13 +1,33 @@
 <div class="post-lists tags">
     <h2>Popular tags</h2>
-    <a href="#" style="font-size: 13px">Tag1</a>
-    <a href="#" style="font-size: 17px">Tag2</a>
-    <a href="#" style="font-size: 13px">Tag3</a>
-    <a href="#" style="font-size: 22px">Tag4</a>
-    <a href="#" style="font-size: 24px">Tag5</a>
-    <a href="#" style="font-size: 26px">Tag6</a>
-    <a href="#" style="font-size: 16px">Tag7</a>
-    <a href="#" style="font-size: 20px">Tag8</a>
-    <a href="#" style="font-size: 28px">Tag9</a>
-    <a href="#" style="font-size: 30px">Tag10</a>
+
+    <?php
+    $sql = "SELECT * FROM tags GROUP BY tags";
+    $res = mysqli_query($con, $sql);
+    while($tag = mysqli_fetch_assoc($res)) {
+        $sqlCount = "SELECT * FROM tags WHERE tags ='$tag[tags]'";
+        $resCount = mysqli_query($con, $sqlCount);
+        $count = mysqli_num_rows($resCount);
+        echo "<a href=\"#\" style=\"font-size: ".tagSize($count)."\">  $tag[tags]($count),  </a>";
+    }
+    function tagSize($count) {
+        if($count > 5) {
+            return "18px";
+        }
+        elseif ($count > 10) {
+            return "24px";
+        }
+        elseif ($count > 15) {
+            return "28px";
+        }
+        elseif ($count > 20) {
+            return "32px";
+        }
+        else {
+            return "13px";
+        }
+
+
+    }
+    ?>
 </div>
